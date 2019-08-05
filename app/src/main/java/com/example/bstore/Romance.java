e298db674d07212d3bb533737467699004b64247
Yasasvi7 <banujaamarasinghe@gmail.com>
2019-08-05 16:31:11 +0530

    First initilizer

---
+++ app/src/main/java/com/example/bstore/Romance.java
@@ -0,0 +1,14 @@
+package com.example.bstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Romance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_romance);
+    }
+}
